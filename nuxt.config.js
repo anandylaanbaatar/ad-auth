22d2b4037ec3bfd0ab381eacff6095033f562e13
Anand Davaasuren <anandylaanbaatar@yahoo.com>
2025-03-29 14:11:10 -0700

    Simplify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
-import { defu } from "defu"
-import { createResolver } from "@nuxt/kit"
-import { defineNuxtConfig } from "nuxt/config"
-const { resolve } = createResolver(import.meta.url)
+// import { defu } from "defu"
+// import { createResolver } from "@nuxt/kit"
+// import { defineNuxtConfig } from "nuxt/config"
+// const { resolve } = createResolver(import.meta.url)
 
 console.log("[Layer] :: Adding AD Auth - v1.2.0")
 
@@ -9,26 +9,34 @@ console.log("[Layer] :: Adding AD Auth - v1.2.0")
  * Config
  */
 
-const config = defineNuxtConfig({
-  components: true,
-})
+// const config = defineNuxtConfig({
+//   components: true,
+// })
 
 /**
  * Middleware
  */
 
-const middlewares = defineNuxtConfig({
-  middleware: [resolve("./middleware/app.global.js")],
-})
+// const middlewares = defineNuxtConfig({
+//   middleware: [resolve("./middleware/app.global.js")],
+// })
 
 /**
  * Modules
  */
 
-const modules = defineNuxtConfig({
+// const modules = defineNuxtConfig({
+//   pinia: {
+//     storesDirs: [resolve("stores/**")],
+//   },
+// })
+
+// export default defu(config, middlewares, modules)
+
+export default defineNuxtConfig({
+  components: true,
+  middleware: ["./middleware/app.global.js"],
   pinia: {
-    storesDirs: [resolve("stores/**")],
+    storesDirs: ["stores/**"],
   },
 })
-
-export default defu(config, middlewares, modules)
